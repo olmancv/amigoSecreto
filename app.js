f4043bc357036592702cb27ed389a547c9d96b46
olmancv <olmancv@gmail.com>
2025-08-05 21:35:57 -0600

    crea funcion ActualizarLista

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //Declaraciones
+const inputAmigo = document.getElementById('amigo');
+const btAgregar = document.getElementById('botonAgrega');
+let listaAmigos = document.getElementById('listaAmigos');
 let amigos = [];
 
+if (inputAmigo && btAgregar ) {
+            inputAmigo.addEventListener('keypress', function(event) {
+                if (event.key === 'Enter' /*|| event.keyCode === 13*/) {
+                    console.log('ENTER');
+                    event.preventDefault();
+                    btAgregar.click();
+                }
+            });
+        } else {
+            if (!inputAmigo) console.error("No se encontró el elemento con ID 'amigo'.");
+            if (!btAgregar) console.error("No se encontró el elemento con ID 'button-add'.");
+        } 
+
+function ActualizarLista() {
+  let listaAmigos = document.getElementById('listaAmigos');
+  listaAmigos.innerHTML = '';
+  
+  amigos.forEach(function (elemento, indice, array) {
+    const nuevoLi = document.createElement('li');   
+    const texto = document.createTextNode(elemento);
+    nuevoLi.appendChild(texto);
+    listaAmigos.appendChild(nuevoLi);
+    //console.log(elemento, indice);
+});
+}
 function agregarAmigo(){
     let nombre = document.getElementById("amigo").value ;
     //let  titulo = document.querySelector('input');  let nombre = titulo.value;
@@ -9,6 +37,7 @@ function agregarAmigo(){
     if (nombre == '') {
         alert("Por favor, ingrese un nombre.");
         document.getElementById("amigo").focus();
+        ActualizarLista();
     } else {
         //agrega el nombre a la lista de amigos
         let indiceNombre = amigos.push(nombre);
